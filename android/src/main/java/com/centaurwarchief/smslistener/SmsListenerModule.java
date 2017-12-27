a4bdde3503b25c2c9b8536ac86f4f0ea298aa389
Jakub Bilko <jakub.bilko@venturedevs.com>
2017-12-27 13:28:56 +0800

    restore method

--- android/src/main/java/com/centaurwarchief/smslistener/SmsListenerModule.java
+++ android/src/main/java/com/centaurwarchief/smslistener/SmsListenerModule.java
@@ -49,6 +49,11 @@ public class SmsListenerModule extends ReactContextBaseJavaModule implements Lif
     }
 
     @Override
+    public void onHostPause() {
+        //unregisterReceiver(mReceiver);
+    }
+
+    @Override
     public void onHostResume() {
         registerReceiverIfNecessary(mReceiver);
     }
